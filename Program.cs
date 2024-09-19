6edbb6c03c675d778d80fea80a04e729e1d83404
Amany Othman <AmanyOthmana@gmail.com>
2024-09-19 11:28:23 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rrepo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, I'm new here");
+
+            Console.ReadKey();
+        }
+    }
+}
